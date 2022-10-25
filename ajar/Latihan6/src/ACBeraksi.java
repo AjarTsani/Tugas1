379b9f73bbf5eef6d96bd0d3c543164775b1f814
AjarTsani <113910134+AjarTsani@users.noreply.github.com>
2022-10-25 14:47:32 +0700

    Add files via upload

---
+++ ajar/Latihan6/src/ACBeraksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class ACBeraksi {
+    void matikanAC(){
+        System.out.println("matikan");
+    }
+    void hidupinAc(){
+        System.out.println("idupin");
+    }
+    void dingininAc(){
+        System.out.println("dinginin");
+       
+    }
+    void panasinAc(){
+        System.out.println("panasin");
+    }
+    
+}
---
+++ ajar/Latihan7/src/Matematika.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Matematika {
+    int tambah(int a, int b){
+         int hasil = a + b;
+         System.out.println("Hasil = " + hasil);
+        return hasil;
+     }
+     int kurang(int a, int b){
+        int hasil = a - b;
+         System.out.println("Hasil = " + hasil);
+        return hasil;
+     }
+     int kali(int a, int b){
+        int hasil = a * b;
+         System.out.println("Hasil = " + hasil);
+        return hasil;
+     }
+     double bagi(double a, double b){
+        double hasil = a / b;
+         System.out.println("Hasil = " + hasil);
+        return hasil;
+     }
+     double tambah(double a, double b, double c){
+         double hasil = a + b + c;
+         System.out.println("hasil = " + hasil);
+         return hasil;
+     }
+     double tambah(double a, double b){
+         double hasil = a + b;
+         System.out.println("hasil = " + hasil);
+         return hasil;
+     }
+     double kurang(double a, double b, double c){
+         double hasil = a - b - c;
+         System.out.println("hasil = " + hasil);
+         return hasil;
+     }
+     double kali(double a, double b, double c){
+         double hasil = a * b * c;
+         System.out.println("hasil = " + hasil);
+         return hasil;
+     }
+     double bagi(double a, double b, double c){
+         double hasil = a / b /c;
+         System.out.println("hasil = " + hasil);
+         return hasil;
+     }
+     
+}
